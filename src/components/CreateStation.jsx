1bedf4e60457b0232e5a5fb5ec23d44e95b6ad5a
Slaven Krilić <skrilic@gmail.com>
2022-02-03 14:59:26 +0100

    ...

--- src/components/CreateStation.jsx
+++ src/components/CreateStation.jsx
@@ -4,7 +4,7 @@ import AddIcon from "@mui/icons-material/Add";
 
 
 
-function CreateStation() {
+function CreateStation(props) {
 	const [station, setStation] = useState({
 		name: "",
 		frequency: ""
@@ -15,24 +15,36 @@ function CreateStation() {
 			name: "",
 			frequency: ""
 		});
+		event.preventDefault();
 	}
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+	
+		setStation((prevStation) => {
+		  return {
+			...prevStation,
+			[name]: value
+		  };
+		});
+	  }
 
 	return (
 		<form className="station-row">
 			<span >
 				<input 
-					id="stationName" 
-					name="stationName" 
+					name="name" 
 					type="text" 
 					placeholder="Station name"
+					onChange={handleChange}
 					value={station.name}
 				/>
 				<input 
-					id="stationFrequency" 
-					name="stationName" 
+					name="frequency" 
 					type="number"
 					step=".01"
 					placeholder="Trasmitting frequency (MHz)"
+					onChange={handleChange}
 					value={station.frequency}
 				/>
 				<button onClick={submitStation}>
