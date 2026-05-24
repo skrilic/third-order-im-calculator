fdeea4bc00dab6d4550a45681276836949e2498a
Slaven Krilic <skrilic@users.noreply.github.com>
2026-05-24 10:47:38 +0200

    feat: integrate Capacitor and update AG Grid dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'third.order.im.calculator',
+  appName: 'third-order-im-calculator',
+  webDir: 'build'
+};
+
+export default config;
