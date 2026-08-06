fe2589292705dcf8933ab9617ff193a680cf7806
Slaven Krilic <skrilic@users.noreply.github.com>
2026-08-06 10:05:41 +0200

    refactor: migrate to Vite and update project structure
    - Updated package.json to use Vite for development and build scripts.
    - Removed old public/index.html and src/index.js files.
    - Added new index.html file with updated structure and script loading.
    - Created src/main.jsx as the new entry point for the application.
    - Updated dependencies to the latest versions, including @mui/material and ag-grid packages.
    - Configured Vite with a basic setup in vite.config.js.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Calculation from './components/Calculation';
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Calculation />);
